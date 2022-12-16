46cced5285a26a383c3b75da92b4e2a377e11b4a
Kajal <113601134+iamKajal24@users.noreply.github.com>
2022-12-16 12:53:53 +0530

    PORTAL

---
+++ about_us.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>About Us</title>
+<link rel="stylesheet" href="css/about_us.css" />
+
+<?php
+include_once"setting/aboutus_navigation.php";
+?>
+</head>
+
+<body>
+<div id="history">
+<p class="p1">The History</p>
+<p class="p2"> INTRODUCTION TO THE UTTARANCHAL UNIVERSITY </p>
+<p class="p3">Uttaranchal University is constituted with the merger of professional institutes of Sushila Devi Centre for Professional Studies & Research Society, namely Law College Dehradun, Uttaranchal Institute of Technology and Uttaranchal Institute of Management.
+
+Uttaranchal University is one of the leading educational hubs of professional courses with innumerable students enrolled in different educational programs. It offers an array of multi–disciplinary courses making it a knowledge portal where excellence matters. In Uttaranchal University, we understand the potential of young minds and nurture them with passion.
+
+Uttaranchal University can boast of being an institution par excellence. It is located in a fascinating geological setting. It is flanked by NH-72 on one side and a beautiful broad slithering river on the other side. The pine trees surrounding the area lend a pristine and wholesome ambience to the campus.
+
+We offer a unique life, you will have access to comprehensive facilities, cultural activities, wide range of academic courses and industry interface</p><br />
+
+<p class="p2">CHANCELLOR'S MESSAGE</p>
+<p class="p3">The founders of Uttaranchal University envisaged an institution of learning which would transcend all barriers and become a university of national character in letter and spirit. Our emphasis is on diffusion of scientific, technical and professional knowledge on one hand and on the other hand building up the character of youth by integrating ethics with education.
+
+I am happy to say that the University’s unique campus and nine interdisciplinary institutes- Law College Dehradun, Uttaranchal Institute of Technology, Uttaranchal Institute of Management, Uttaranchal Institute of Pharmaceutical Sciences, Uttaranchal College of Applied & Life Sciences, School of Agriculture, University Polytechnic, Uttaranchal College of Journalism & Mass Communication and School Of Hospitality Management are making their mark at the state and national level in the frontier areas of Law, Engineering, Management, Information Technology, Pharmacy, Applied & Life Sciences, Agricultural Science, Journalism & Mass Communication and Hotel Management. I hope the same for School of Liberal Arts.
+
+The University is complemented by the pioneering efforts of more than a decade in legal education, engineering and management which has won us the place among the premier institutes of learning in the country. Our dedication to offer the best infrastructure and faculty has been amply rewarded by the excellent academic results achieved by our students. Among other accolades, our students have won prestigious awards from many Universities. A University can grow when research is combined with teaching. Therefore we have ensured the development of a collaborative environment conducive to learning, exposure to the best international practices and promotion of innovation and creativity.
+
+<br>Our students will find learning in Uttaranchal University an invigorating experience.</br>
+<br>I extend my warm welcome to our new entrants.</br>
+
+<br>Jitender Joshi</br>
+<br>Chancellor</p><br />
+<p class="p2">VISION OF UTTARANCHAL UNIVERSITY	</p>
+<p class="p3">The vision of Uttaranchal University the following:
+<ul class="ul2">
+	<li>Promoting uttaranchal university's graduate programs</li>
+	<li>Facilitate the appointment of supervisor for graduate student</li>
+	<li>Oversees the provision of financial assistance</li>
+	<li>Coordinate the end of semester appraisal of candidature</li>
+	<li>Organise the mid-term candidature review </li>
+	<li>Coordinate the assessment for graduation purposes</li>
+	<li>Formulate administrative policies pertaining to graduate studies</li>
+	<li>To be a world-class University in providing pre-eminent education experiences those are stimulating, responsive to the needs of the 21st century, nurtures talent and transforms the lives of millions through excellence in teaching, learning, training and research.</li>
+	<li>To develop a Centre of Excellence in academics and education as well as research at par with national and international standards. Reach a position of distinction by offering first-class education and serving the community in relevant areas of interest.</li>
+</ul>
+</p>
+<br><br><br>
+<br /><hr color="peru" size="4"/>
+</div>
+<br><br><br>
+<div id="organization_chart">
+<p class="p1">Organisations</p>
+<p class="about_image"><img src="image/organization_chart.jpg"/></p>
+<br><br><br><br>
+</div>
+</body>
+</html>
---
+++ contact_us.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Contact Us</title>
+<link rel="stylesheet" href="css/contact_us.css" />
+
+<?php
+include_once"setting/contactus_navigation.php";
+?>
+</head>
+
+<body>
+<div id="contact">
+<p class="p1">Contact</p>
+<p class="p2">Address of Uttaranchal University</p>
+<p class="p3">Arcadia Grant, P.O. Chandanwari,<br>Premnagar, Dehradun<br> Uttarakhand-248007<br>India.<br><br></p>
+<table>
+<tr>
+	<td>
+			<p class="p2">Contact us at:</p>
+			<p class="p3">Tel- 0135-2772135, 2771170, 2773531</p>
+			<p class="p3">Law College Tel– 0135-2770302, 2770303, 2770354</p>
+			<p class="p3">UIT Tel– 0135- 2772135, 2770315, 2770316,2770317 </p>
+			<p class="p3">Fax-2772231, 2771461</p>
+			<p class="p3">Toll Free- 18002700288, 18001804201, 18001804221</p>
+			<p class="p3">UIM Fax– 2772231 </p>
+	</td>		
+	<td >
+    <br /><br /><p class="p2">Email:</p><p class="p3">university@uttaranchaluniversity.ac.in</p><br /><br />
+    <p class="p2">Our Social Pages:</p>
+	<p class="p3"> <a href="https://www.facebook.com/Uttaranchal University/"> <img src="pictures/facebook.png" width="40" height="40"></a> <a href="https://www.instagram.com/umpmalaysia/"> <img src="pictures/instagram.png" width="55" height="40"></a> <a href="https://twitter.com/fskkp?lang=en"><img src="pictures/twitter.png" width="40" height="40"></a></p><br /><br />
+    </td>
+</tr>
+</table>
+
+</div>
+<br><br><br>
+<br /><hr color="peru" size="4"/>
+<br>
+<div id="faq">
+<p class="p1">FAQ</p>
+<p class="p2"><br>Who are our Alumni?</p>
+<p class="p3">All individuals who have followed Diploma, Undergraduate, Postgraduate or PhD of the programmes of Uttaranchal University for the duration of at least one semester at uttaranchal University can register as our alumni.</p>
+<p class="p2"><br>How do I become a member of the Uttaranchal UNiversity?</p>
+<p class="p3">All undergraduates and post graduates of Uttaranchal UNiversity can register as our alumni. It is important for alumni to update their particulars for the Uttaranchal University Alumni Directory (Database) by register as a member online and pay at the counter of Alumni Uttaranchal University so that we can keep in touch with you for life.</p>
+<p class="p2"><br>Do I need to pay any fee?</p>
+<p class="p3">Yes, but it is only for membership registration. You can choose to pay yearly or pay for Life Membership. The fee for yearly paid and life membership are Uttaranchal University respectively. </p>
+<p class="p2"><br>As an alumni, how can I contribute towards the University?</p>
+<p class="p3">You can help by participating in the various activities organized by the University or by contributing ideas or programmes towards enriching learning experience of all students. Other assistance include donation, sponsorship, expertise and volunteer services. Whatever your contribution they are most welcome.</p>
+<p class="p2"><br>I am still a student of the University, can I take part in alumni activities?</p>
+<p class="p3">Yes, of course. We believed that the bond between alumni and students should begin as soon as the student enters the University because you will become our future alumni later on. The Alumni Advancement is also committed in organising training especially in soft skills and career development programmes to raise your employability in the job market upon graduation. We hope this will instill a pride in your alma mater and later on may wish to make whatever contribution towards achieving excellence for the University.
+</p>
+</div>
+<br><br><br><br>
+</body>
+</html>
